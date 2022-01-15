f0cbcb14af459e56cd06ca2c8b8c35f618c8adbb
Henrique Cecconi <henrique.cecconi@gmail.com>
2022-01-15 10:48:55 -0300

    Creating a basic guessing game. Initially just recieving the users guess.

---
+++ guessing_game.py
@@ -0,0 +1,8 @@
+print("*****************************")
+print("Welcome to the guessing game!")
+print("*****************************")
+
+secret_number = 42
+
+guess = input("Insert your guess: ")
+print("You guessed", guess)
