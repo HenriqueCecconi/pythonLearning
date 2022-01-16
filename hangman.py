9f3c492da140070bf6735daaf783866f39a9ab06
Henrique Cecconi <henrique.cecconi@gmail.com>
2022-01-16 12:41:03 -0300

    Exhibiting guesses and current state of the word to be guessed

--- hangman.py
+++ hangman.py
@@ -4,16 +4,19 @@ def play():
     print("****************************")
 
     secret_word  = "banana"
+    letters_guessed = ["_", "_", "_", "_", "_", "_"]
+
     hanged = False
     guessed = False
     
     while(not hanged and not guessed):
+        print(letters_guessed)
         guess = input("Take a guess: ")
         guess = guess.strip()
         index = 0
         for letter in secret_word:
             if(guess.upper() == letter.upper()):
-                print("Found the letter {} in the position {}".format(letter, index))
+                letters_guessed[index] = letter
             index += 1
 
     print("End of game")
