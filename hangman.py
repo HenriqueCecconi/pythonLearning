543d3ce0e79181feb5a497741a1c665c35cca507
Henrique Cecconi <henrique.cecconi@gmail.com>
2022-01-16 12:53:52 -0300

    Implementing winning and losing to the game

--- hangman.py
+++ hangman.py
@@ -3,21 +3,33 @@ def play():
     print("Welcome to the hangman game!")
     print("****************************")
 
-    secret_word  = "banana"
+    secret_word  = "banana".upper()
     letters_guessed = ["_", "_", "_", "_", "_", "_"]
 
     hanged = False
     guessed = False
+    errors = 0
     
     while(not hanged and not guessed):
         print(letters_guessed)
         guess = input("Take a guess: ")
-        guess = guess.strip()
-        index = 0
-        for letter in secret_word:
-            if(guess.upper() == letter.upper()):
-                letters_guessed[index] = letter
-            index += 1
+        guess = guess.strip().upper()
+        if(guess in secret_word):
+            index = 0
+            for letter in secret_word:
+                if(guess == letter):
+                    letters_guessed[index] = letter
+                index += 1
+        else:
+            errors += 1
+
+        hanged = (errors == 6)
+        guessed = ("_" not in secret_word)
+
+    if(guessed):
+        print("You win!")
+    else:
+        print("You lose!")
 
     print("End of game")
 
